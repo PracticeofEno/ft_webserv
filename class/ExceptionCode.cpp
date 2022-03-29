7dd039d257fc16574a2b200c8c18835725192ff6
chicjhpark <chicjhpark@gmail.com>
2022-03-29 19:50:41 +0000

    parseChunked

--- class/ExceptionCode.cpp
+++ class/ExceptionCode.cpp
@@ -56,14 +56,10 @@ void ExceptionCode::handleException()
         res.file_path_ = location.getFilePath("/" + server.error_page_);
         res.send(con_.socket_);
     }
-<<<<<<< HEAD
-    // else if (_code ==
-=======
     else if (_code == 302)
     {
         res.header_["Location"] = location_;
         res.writeStartLine(con_.socket_);
         res.writeHeader(con_.socket_);
     }
->>>>>>> 74b04afb5249453ebd8c0c999e7aa81c94fd60b8
 }
